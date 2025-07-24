c427dc84aaa36e5e5ad35cce74c59ac1ea260f47
pasuzw <pasuzw@gmail.com>
2025-07-25 08:02:23 +0930

    blink led 1 - using rust core

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::ptr::write_volatile;
+
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use panic_halt as _;
@@ -10,12 +12,25 @@ use rtt_target::{rprintln, rtt_init_print};
 fn main() -> ! {
     // rtt_init_print!();
     // rprintln!("Hello from RTT");
-    let mut x: usize = 0;
+    const GPIO0_PINCNF21_ROW1_ADDR: *mut u32 = 0x5000_0754 as *mut u32;
+    const GPIO0_PINCNF28_COL1_ADDR: *mut u32 = 0x5000_0770 as *mut u32;
+    const DIR_OUTPUT_POS: u32 = 0;
+    const PINCNF_DRIVE_LED: u32 = 1 << DIR_OUTPUT_POS;
+    unsafe {
+        write_volatile(GPIO0_PINCNF21_ROW1_ADDR, PINCNF_DRIVE_LED);
+        write_volatile(GPIO0_PINCNF28_COL1_ADDR, PINCNF_DRIVE_LED);
+    }
+    const GPIO0_OUT_ADDR: *mut u32 = 0x5000_0504 as *mut u32;
+    const GPIO0_OUT_ROW1_POS: u32 = 21;
+    let mut is_on: bool = false;
     loop {
-        x += 1;
         // rprintln!("Echo...");
-        for _ in 0..x {
+        unsafe {
+            write_volatile(GPIO0_OUT_ADDR, (is_on as u32) << GPIO0_OUT_ROW1_POS);
+        }
+        for _ in 0..400_000 {
             nop();
         }
+        is_on = !is_on;
     }
 }
