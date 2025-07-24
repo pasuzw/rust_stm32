c0dd84fd0bf09341681b34673cc9d7e18e4bae6e
pasuzw <pasuzw@gmail.com>
2025-07-24 19:50:42 +0930

    feat: Use RTT to say Hello

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 #![no_std]
 #![no_main]
 
-
+use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-
+use panic_halt as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
-fn main() -> !{
-    loop {}
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello from RTT");
+    loop {
+        rprintln!("Echo...");
+        for _ in 0..100_000 {
+            nop();
+        }
+    }
 }
